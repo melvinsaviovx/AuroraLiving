ae3ff1733af354a6f50caf1c3e175e902466a743
Melvin Savio <melvinsaviovx@gmail.com>
2025-08-27 20:49:24 +0530

    initial commit

---
+++ src/app/Component/header/header.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.html',
+  styleUrl: './header.css'
+})
+export class Header {
+isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+    const nav = document.querySelector('.nav-links');
+    if (nav) {
+      nav.classList.toggle('active', this.isMenuOpen);
+    }
+  }
+}
