09b7406bbe43084d984d25ac198bb3793b7091d5
Melvin Savio <melvinsaviovx@gmail.com>
2025-08-28 20:19:30 +0530

    added home page

---
+++ src/app/Component/team/team.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-team',
+  standalone: true,
+  templateUrl: './team.html',
+  styleUrl: './team.css'
+})
+export class Team {
+
+}
--- src/app/app.ts
+++ src/app/app.ts
@@ -2,9 +2,14 @@ import { Component, signal } from '@angular/core';
 import { Header } from './Component/header/header';
 import { Footer } from './Component/footer/footer';
 import { Home } from './Component/home/home';
+import { Services } from './Component/services/services';
+import { Team } from './Component/team/team';
+import { Portfolio } from './Component/portfolio/portfolio';
+import { Contact } from './Component/contact/contact';
+
 @Component({
   selector: 'app-root',
-  imports: [Header,Footer,Home],
+  imports: [Header,Footer,Home,Services,Team,Portfolio,Contact],
   standalone: true,
   templateUrl: './app.html',
   styleUrl: './app.css'
